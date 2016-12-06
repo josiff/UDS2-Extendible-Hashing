674233f435b822f8c8ae5a1d7cca26c90d635c1f
Olga Chovancova <chovancova.olga@yahoo.com>
2016-12-06 17:30:22 +0100

    Uprava - blocku a podobne.

---
+++ Solution/DataStructuresLibrary/Extendible-Hashing/NeusporiadanySubor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.Extendible_Hashing
+{
+    /// <summary>
+    /// Trieda - Neusporiadany subor. 
+    /// </summary>
+    public class NeusporiadanySubor
+    {
+        #region Vlastnosti
+        public int 
+
+        #endregion
+
+
+        #region Metody
+
+        #endregion
+
+    }
+}
